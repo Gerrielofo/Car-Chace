eefbd3bd80bb5339f4aec431b1d61c54f3906b5f
Douche-Box <95449433+Douche-Box@users.noreply.github.com>
2024-02-26 16:48:24 +0100

    cr rd

--- CarChace/Assets/Scripts/Movement/CarController.cs
+++ CarChace/Assets/Scripts/Movement/CarController.cs
@@ -7,10 +7,19 @@ public class CarController : MonoBehaviour
     [SerializeField] private PlayerInput playerInput = null;
     public PlayerInput PlayerInput => playerInput;
 
+    [SerializeField] WheelCollider _leftFront;
+    [SerializeField] WheelCollider _rightFront;
+    [SerializeField] WheelCollider _leftBack;
+    [SerializeField] WheelCollider _rightBack;
+
     [SerializeField] float _isMove;
-    // [SerializeField] float _isTurn;
     [SerializeField] float _isTurn;
 
+    [SerializeField] float _steerAngle = 25f;
+    [SerializeField] float steerAngle;
+
+
+    [SerializeField] float _speed;
     [SerializeField] float _wheelRotationSpeed;
 
     [SerializeField] Vector3 _frontWheelsRotation;
@@ -50,6 +59,11 @@ public class CarController : MonoBehaviour
 
     private void Update()
     {
+        _leftBack.motorTorque = _isMove * _speed;
+        _rightBack.motorTorque = _isMove * _speed;
 
+        steerAngle = _steerAngle * _isTurn;
+        _leftFront.steerAngle = steerAngle;
+        _rightFront.steerAngle = steerAngle;
     }
 }
