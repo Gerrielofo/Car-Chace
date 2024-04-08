bbb80c06aa5b4a38fd5ac61715bc94378114beb2
Gerrielofo <gerlof.reinink@gmail.com>
2024-04-08 14:45:15 +0200

    Boat Navmesh

--- CarChace/Assets/Scripts/Boat.cs
+++ CarChace/Assets/Scripts/Boat.cs
@@ -7,12 +7,24 @@ public class Boat : MonoBehaviour
 {
     NavMeshAgent _navMeshAgent;
 
+    Transform _boatFront;
+
     [Header("Settings")]
     [SerializeField] float _desiredSpeed;
     [SerializeField] float _fov;
+    [SerializeField] float _randomDestinationRadius;
+
+    [SerializeField] LayerMask _waterMask;
+
+    [Header("Debug")]
+    public bool ShowGizmos;
+
+    [SerializeField] float _minDistanceFromPoint;
+    float _distanceFromPoint;
 
     [Header("Info")]
     [SerializeField] bool _hasOpened;
+
 
     void Start()
     {
@@ -23,12 +35,35 @@ public class Boat : MonoBehaviour
 
     void Update()
     {
+        if (_navMeshAgent.destination != transform.position)
+        {
+            _distanceFromPoint = Vector3.Distance(transform.position, _navMeshAgent.destination);
+        }
 
+        if (_distanceFromPoint < _minDistanceFromPoint)
+        {
+            _navMeshAgent.destination = GetRandomDestination();
+        }
     }
 
-    void GetRandomDestination()
+    private bool CheckForAngle(Vector3 point, Vector3 boat, Vector3 direction) //calculates the angle between the boat and the postion 
     {
+        Vector3 distance = (point - boat).normalized;
+        float CosAngle = Vector3.Dot(distance, direction);
+        float Angle = Mathf.Acos(CosAngle) * Mathf.Rad2Deg;
 
+        if (Angle < _fov)
+            return true;
+        else
+            return false;
+    }
+
+    Vector3 GetRandomDestination()
+    {
+        Vector3 direction = new Vector3(0, Random.Range(-_fov + transform.rotation.y, _fov + transform.rotation.y), 0);
+        // Vector3 _randomDirection = Random.insideUnitSphere * _randomDestinationRadius;
+        NavMesh.SamplePosition(direction, out NavMeshHit hit, _randomDestinationRadius, _waterMask);
+        return hit.position;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -39,6 +74,27 @@ public class Boat : MonoBehaviour
         {
             bridgeTrigger.ToggleBridge(!_hasOpened);
             _hasOpened = !_hasOpened;
+        }
+    }
+
+    private void OnDrawGizmos() // shows a Gizmos representing the waypoints and AI FOV
+    {
+        if (ShowGizmos == true)
+        {
+            CalculateFOV();
+        }
+
+        void CalculateFOV()
+        {
+            Gizmos.color = Color.white;
+            float rayRange = 10.0f;
+            float halfFOV = _fov / 2.0f;
+            Quaternion leftRayRotation = Quaternion.AngleAxis(-halfFOV, Vector3.up);
+            Quaternion rightRayRotation = Quaternion.AngleAxis(halfFOV, Vector3.up);
+            Vector3 leftRayDirection = leftRayRotation * transform.forward;
+            Vector3 rightRayDirection = rightRayRotation * transform.forward;
+            Gizmos.DrawRay(_boatFront.position, leftRayDirection * rayRange);
+            Gizmos.DrawRay(_boatFront.position, rightRayDirection * rayRange);
         }
     }
 }
